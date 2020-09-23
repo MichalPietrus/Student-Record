7a27525c3044b8beb1cad84e47e73740d127ae61
MichalPietrus <grahul2121@gmail.com>
2020-09-23 23:17:15 +0200

    Cleared Project from unused imports

--- src/main/java/com/studentrecord/web/UserRegistrationController.java
+++ src/main/java/com/studentrecord/web/UserRegistrationController.java
@@ -3,7 +3,6 @@ package com.studentrecord.web;
 import com.studentrecord.model.User;
 import com.studentrecord.service.UserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
